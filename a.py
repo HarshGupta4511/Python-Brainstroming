7c9b82132994698a84c370248f621bbf4b8c66f1
Harsh Gupta <guptaharshk123@gmail.com>
2026-08-09 21:48:20 +0530

    Add files via upload
    Easy Problems

---
+++ a.py
@@ -0,0 +1,100 @@
+# def is_even(n):
+#     if n%2 == 0:
+#         return True
+#     else :
+#         return False
+
+# print(is_even(41))
+
+# def sum_n(n):
+#     sum = 0
+#     for i in range(1,n+1):
+#        sum = sum + i 
+
+#     return sum
+
+# print(sum_n(3))
+
+# def fac(n):
+#     mul = 1 
+#     for i in range(1,n+1):
+#         mul = mul * i
+#     return mul
+
+# print(fac(4))
+
+# def find_min(lst):
+#     min = lst[0]
+#     for i in lst:
+#         if i < min:
+#             min = i
+#     return min
+
+# print(find_min([3,5,2,6,1,77]))
+
+# def count_even(lst):
+#     c = 0
+#     for i in lst:
+#         if i%2 == 0:
+#             c = c +1
+#     return c
+        
+# print(count_even([12,34,22,55,11,34,66]))
+
+# def sum_list(lst):
+#     sum = 0
+#     for i in lst:
+#         sum = sum + i
+
+#     return sum
+
+# print(sum_list([1,2,4,5,6,32]))
+
+# def check_num (lst , x):
+#     for i in lst:
+#         if i == x:
+#             return "Found"
+#         else:
+#             return "Not Found"
+    
+#     return
+
+# print(check_num([1,2,4,5,2,3,4],12))
+
+# def mul_list(lst):
+#     mul = 1
+#     for i in lst:
+#         mul = mul * i
+
+#     return mul
+
+# print(mul_list([1,2,4]))
+# l1 = []
+# def reverse_list(lst):
+#     for i in range (-len(lst) , 0 ):
+        
+#         k = lst[i]
+#         l1.append(k)
+
+#     return l1
+
+# print(reverse_list([1,2,3,4,6,7]))
+
+# l1 = []
+# def reverse_list(lst):
+#     f = len(lst)
+#     for i in lst:
+#         if f > -1:
+#             f = f-1
+#             k = lst[f]
+#             l1.append(k)
+             
+#     return l1
+
+# print(reverse_list([1,2,3]))
+kk = [1,2,3,5]
+k = 4
+print(type(k))
+print(type(kk))
+        
+
