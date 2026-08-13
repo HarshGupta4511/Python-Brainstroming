decdc408c3332167e8d2a2a653a876f86111a004
Harsh Gupta <guptaharshk123@gmail.com>
2026-08-13 21:43:42 +0530

    Add files via upload

---
+++ h.py
@@ -0,0 +1,13 @@
+
+# def is_prime(n):
+#     for i in range(2, n):     
+#         if n%i == 0:
+#             return False
+
+#     return True
+            
+
+# print(is_prime(19))
+
+for i in range(9):
+    print(i)
